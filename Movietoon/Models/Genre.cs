66829b6ea58df556a28a122f85af972ef25216ff
Ivan Narvaez <ivanrnarvaez@gmail.com>
2020-11-23 12:29:42 -0500

    Adding base classes Customer, Movie, Genre

---
+++ Movietoon/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace Movietoon.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string GenretName { get; set; }
+    }
+}
---
+++ Movietoon/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace Movietoon.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public short ReleaseYear { get; set; }
+        public DateTime DateAdded { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
