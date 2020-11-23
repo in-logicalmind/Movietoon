05c944ac8a4c65730759afab0aa8f9e5b6474828
Ivan Narvaez <ivanrnarvaez@gmail.com>
2020-11-23 11:50:36 -0500

    Add project files.

---
+++ Movietoon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movietoon.Startup))]
+namespace Movietoon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
