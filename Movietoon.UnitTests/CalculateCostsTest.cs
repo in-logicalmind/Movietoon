f53bb2e7d5d4348df0a05d5f1ad8670250c66baa
Ivan Narvaez <ivanrnarvaez@gmail.com>
2020-11-25 09:47:02 -0500

    Added Unit Testing project Test calculateCost method

---
+++ Movietoon.UnitTests/CalculateCostsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Movietoon.Models;
+
+namespace Movietoon.UnitTests
+{
+    [TestClass]
+    public class CalculateCostsTest
+    {
+        [TestMethod]
+        public void CalculateCostsMovie_ValidMembership_returnsValue()
+        {
+            var calculatedCosts = new CalculateCosts();
+
+            //Arrange
+            var result = calculatedCosts.CalculateCostsMovie(
+                new MembershipType {Description = "Quarterly", DiscountPct = 20, Months = 3},
+                new Movie {Price = 1, Title = "Test movie"});
+
+            var expected = 0.8;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Movietoon.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Movietoon.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Movietoon.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b012f1d-8f04-4bfd-b884-79bd220c47b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
